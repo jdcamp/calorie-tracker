3c9a7ef3238295785a8c6fce4df58055b8b054a8
jdc <jdcxxx@gmail.com>
2017-03-31 13:03:44 -0700

    create new-food component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { FoodListComponent } from './food-list/food-list.component';
+import { AddFoodComponent } from './add-food/add-food.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FoodListComponent
+    FoodListComponent,
+    AddFoodComponent
   ],
   imports: [
     BrowserModule,
